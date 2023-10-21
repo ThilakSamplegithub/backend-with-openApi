969a8242f233f07f91c13e1d2082b3aa2dbff6dd
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2023-10-21 19:57:43 +0530

    now i got api key

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express=require("express")
 require('dotenv').config()
+const OpenAI = require("openai")
 const axios = require('axios')
 const app=express()
 app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("welcome")
 })
-const apiKey = process.env.OPENAI_API_KEY; // Replace with your actual API key
+console.log(process.env.OPENAI_API_KEY)
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// // const apiKey = process.env.OPENAI_API_KEY; // Replace with your actual API key
+// app.get("/quote", async (req, res) => {
+//   try {
+//     const { keyword, type, words } = req.query;
+//     console.log(req.query);
+//     const chatCompletion = await openai.chat.completions.create({
+//       messages: [
+//         {
+//           role: "system",
+//           content: `Please provide me with a ${type} with respect to ${keyword} with maximum ${words} words`,
+//         },
+//       ],
+//       model: "gpt-3.5-turbo",
+//     });
+
+//     const shayari = chatCompletion.choices[0].message;
+//     res.send({ message: shayari.content });
+//   } catch (error) {
+//     res.status(400).send({ error: error });
+//   }
+// });
 
 app.post('/generate-response', async (req, res) => {
   try {
@@ -19,7 +44,7 @@ console.log(userMessage,'is userMessage')
       max_tokens: 50, // Adjust this as needed
     }, {
       headers: {
-        'Authorization': `Bearer ${apiKey}`
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       }
     });
   console.log(response.data)
